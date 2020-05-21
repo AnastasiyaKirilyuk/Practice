ba1cc4cd41b4b49ccc10acf6c13f126c7a66656e
AnastasiyaKirilyuk <47120086+AnastasiyaKirilyuk@users.noreply.github.com>
2020-05-21 08:07:48 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,125 @@
+//ES5 (Ecmascript).....ES 10
+
+// Data Types
+// 1. String - строка
+// 2. Number - число
+// 3. boolean - логический тип
+// 4. null
+// 5. underfined
+// 6. object - объект
+// console.log(123);
+
+// console.log(typeof 'Text'); - string
+//console.log(typeof 2020); - number
+//console.log(typeof false); - boolean
+// console.log(true, false);
+// alert (123);
+//prompt('How?', 20);
+
+// 2 задача:
+//  for (var i=1; i<=100; i+=1) {
+//      if (i % 3 === 0) {
+//          console.log(i,'Fizz');
+//      }
+//      if (i % 5 === 0){
+//          console.log(i,'Buzz');
+//      }
+//      if (i % 15 === 0){
+//          console.log(i,'FizzBuzz');
+//      }
+//  }
+
+// 2.1 задача:
+// for (var i=1; i<=100; i+=1) {
+//       if (i % 3 === 0 && i % 5 === 0) {
+//           console.log(i,'FizzBuzz');
+//      }
+//       if (i % 3 === 0) {
+//           console.log(i,'Fizz');
+//       if (i % 5 === 0){
+//           console.log(i,'Buzz');
+//      }
+//     }
+//     }
+
+
+//3 задача - :) :)
+   var i = 45; 
+   while(i <= 670){
+     if (i % 10 === 0){
+       console.log(i);
+   }
+   i += 5;
+  }
+
+//   var i = 45; 
+//   while(i <= 670){
+//       console.log(i, i % 10 === 0);
+//         i += 5;
+  
+//  }
+  
+
+//4 задача - :) :)
+//  var i = 67; 
+//    while(i >= 42){
+//        console.log(i, i % 2 !== 0);
+//          i -= 1;
+//    }
+
+
+//for(var i = 0; i <= 10; i +=4) {
+//    console.log(i);
+//}
+
+// задача 5.3
+//  for(var i = 45; i <= 670; i +=1) {
+//      if (i % 10 === 0){
+//      console.log(i);
+//      }
+//  }
+
+// задача 5.4
+  // for(var i = 67; i >= 45; i -= 1) {
+  //     if (i % 2 !== 0){
+  //     console.log(i);
+  //     }
+  // }
+
+// задача 1
+//  function Degrees(x){
+//    var result = x*1.8+32;
+// //     //for (var x=-100; x>=100; x += 1)
+//    console.log(result);
+//    return result;
+//  }
+//   Degrees(1);
+//   Degrees(2);
+//   Degrees(3);
+//   Degrees(4);
+//   Degrees(5);
+//   Degrees(6);
+//   Degrees(7);
+//   Degrees(8);
+//   Degrees(9);
+//   Degrees(10);
+//   Degrees(11);
+//   Degrees(12);
+//   Degrees(13);
+//   Degrees(14);
+//   Degrees(15);
+//   Degrees(16);
+//   Degrees(17);
+//   Degrees(18);
+//   Degrees(19);
+//   Degrees(20);
+
+// задача 1 (2)
+// function Degrees(start, end){
+// var i;
+// for(i = start; i <= end; i+=1) {
+//   if(i*1.8+32); }
+// }
+
+
+// Degrees (-100, 100);
